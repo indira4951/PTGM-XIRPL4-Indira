c7dc72c6991c960ce74a96706b17712801c903b2
indira4951 <dasilvagabriellasjr@gmail.com>
2026-08-08 17:12:56 +0700

    Add files via upload

---
+++ Try1.py
@@ -0,0 +1,11 @@
+Nama = "Indira"
+Hobi = "Nganggur"
+Umur = 15
+Perempuan = "True"
+BeratBadan = 40
+print("===Biodata===")
+print("Nama =", Nama)
+print ("Hobi =", Hobi)
+print("Umur =", Umur, "Tahun")
+print ("Perempuan =", Perempuan)
+print("Berat Badan =", BeratBadan)
---
+++ Try2.py
@@ -0,0 +1,4 @@
+kopi = 15000
+rokok = 30000
+roti = 14000
+print("total pembayaran = Rp.", kopi + rokok + roti)
